e796a1d0539499372ba773044b291cd55834359e
Ruben Giannotti <ruben.giannotti@gmx.net>
2020-04-27 03:22:19 +0200

    Added method: bfs

--- src/index.ts
+++ src/index.ts
@@ -3,9 +3,11 @@ import TypeInsurance from 'type-insurance';
 type tuple = [string, string];
 type nodes = string | string[];
 type edges = tuple | tuple[];
+type adjacencyList = Map<string, string[]>;
+type lookupResult = [number, string[]] | undefined;
 
 export default class Undirected {
-	public graph: Map<string, string[]>;
+	public graph: adjacencyList;
 
   constructor(nodes: nodes, edges: edges) {
 		this.graph = new Map();
@@ -26,5 +28,38 @@ export default class Undirected {
 			this.graph.get(key1).push(key2);
 			this.graph.get(key2).push(key1);
 		});
+	}
+
+	bfs(
+		list: adjacencyList,
+		search: string,
+		start: string,
+		callback = (x: string, y: number) => {},
+	): lookupResult {
+		const queue = [start];
+		const checked = new Set();
+		const path = [];
+		
+		while(queue.length > 0) {
+			const _current = queue.shift();
+			const _nodes = list.get(_current);
+			
+			for(const node of _nodes) {       
+				if(node === search) {
+					const _path = [start, ...path, search];
+					const { number } = new TypeInsurance(_path.length - 1)
+					callback(search, number);
+					return [number, _path];
+				}
+				
+				if(!checked.has(node)) {
+					checked.add(node);
+					queue.push(node);
+					path.push(node);
+				}
+			}
+		}
+		
+		return undefined;
 	}
 }
