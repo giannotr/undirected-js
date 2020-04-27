3e2c597aa812d6688cc3958177286c816aba2d7d
Ruben Giannotti <ruben.giannotti@gmx.net>
2020-04-27 03:11:02 +0200

    Added method: addEdges

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,30 @@
 import TypeInsurance from 'type-insurance';
 
-export default class Undirected {
-	public graph: Map;
+type tuple = [string, string];
+type nodes = string | string[];
+type edges = tuple | tuple[];
 
-  constructor(nodes: string | any[], edges: any[][]) {
+export default class Undirected {
+	public graph: Map<string, string[]>;
+
+  constructor(nodes: nodes, edges: edges) {
 		this.graph = new Map();
 		
 		this.addNodes(nodes);
+		this.addEdges(edges);
 	}
 
-	addNodes(keys: any) {
+	addNodes(keys: nodes) {
 		const { array } = new TypeInsurance(keys);
-		array.forEach((key: any) => this.graph.set(key, []));
+		array.forEach((key: string) => this.graph.set(key, []));
+	}
+
+	addEdges(pairs: edges) {
+		const matrix = (new TypeInsurance(pairs)).array;
+		matrix.forEach((pair: tuple) => {
+			const [key1, key2] = pair;
+			this.graph.get(key1).push(key2);
+			this.graph.get(key2).push(key1);
+		});
 	}
 }
